04e29b1ae089f81ebc1601c28d240cc0a3256040
l4dno <ves.artem2005@gmail.com>
2025-03-10 23:47:50 +0300

    Временные файлы для 17-го задания

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from orm import handler
 from windows.greetWindow import GreeterWindow
-
-h = handler("database.db")
-
 
 greeter = GreeterWindow()
 greeter.mainloop()
--- windows/customerWindow.py
+++ windows/customerWindow.py
@@ -1,7 +1,31 @@
+import orm
 from window import window
-import tkinter
+from tkinter import *
 from tkinter import ttk
 
 class CustomerWindow:
     def __init__(self):
-        w = window("Меню покупателя", (800, 600))
+        self.h = orm.handler("database.db")
+
+        self.w = window("Меню покупателя", (600, 400))
+
+        tableheading = ttk.Label(self.w.root, text="выберите товар", font=("Segoe UI bold", 16))
+        tableheading.pack(expand=True)
+
+        mainframe = ttk.Frame(self.w.root)
+        mainframe.pack(expand=True)
+
+        self.productsTable = ttk.Treeview(mainframe, columns=("name", "price", "category"), show="headings")
+        self.productsTable.heading("name", text="Название")
+        self.productsTable.heading("price", text="Цена")
+        self.productsTable.heading("category", text="Категория")
+        self.productsTable.pack()
+
+        self.updateTableData()
+
+    def updateTableData(self):
+        products = self.h.selectAll()
+
+        for product in products:
+            woid = product[1:]
+            self.productsTable.insert("", END, values=woid)
