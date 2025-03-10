ac5ec4cf5c7ed911b4994b168487b87df4c3fa1d
l4dno <ves.artm2005@gmail.com>
2025-03-10 22:20:48 +0300

    Инициализировал промежуточную ветку

--- windows/customerWindow.py
+++ windows/customerWindow.py
@@ -5,6 +5,3 @@ from tkinter import ttk
 class CustomerWindow:
     def __init__(self):
         w = window("Меню покупателя", (800, 600))
-
-        itemsTable = ttk.Treeview(w.root, columns=["id", "name", "price", "category"])
-        itemsTable.pack(side="top")
